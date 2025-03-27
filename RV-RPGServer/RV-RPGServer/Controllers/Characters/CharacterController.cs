ca3e01955fe6f20b2823237d54da12c4e19acf36
Roman Vitolo <rvitolo@7r1ck.com>
2025-03-27 06:41:00 -0300

    init

---
+++ RV-RPGServer/RV-RPGServer/Controllers/Characters/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RV_RPGServer.Entities;
+
+namespace RV_RPGServer.Controllers.Characters
+{
+    [ApiController]
+    [Route("api/characters")]
+    public class CharacterController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Character> Get()
+        {
+            return new List<Character>
+            {
+                new Character{Id = 1, Name = "Paladin"},
+                new Character{Id = 2, Name = "Mage"},
+            };
+        }
+    }
+}
---
+++ RV-RPGServer/RV-RPGServer/Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RV_RPGServer.Entities;
+
+namespace RV_RPGServer.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+        
+        public DbSet<Character> Characters { get; set; }
+    }
+}
